2cf75c089ed766f618a36cb41ea4648a133f8236
TudoHuang <88125758+tudohuang@users.noreply.github.com>
2024-07-19 17:10:38 +0800

    Update DAC_10ms.cpp

--- Cscripts/DAC_10ms.cpp
+++ Cscripts/DAC_10ms.cpp
@@ -1,9 +1,9 @@
 #include "DAC_10ms.h"
 #include <Arduino.h>
 
-void dac() {
+void dac(int pin) {
     for (int i = 0; i < 256; i++) {
-        dacWrite(25, i);
+        dacWrite(pin, i);
         delayMicroseconds(5000.0 / 128.0);
     }
 }
