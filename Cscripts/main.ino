5d94af4bea6894a0bad182c8bf7d332dbb5e305f
TudoHuang <88125758+tudohuang@users.noreply.github.com>
2024-07-19 17:17:02 +0800

    Update main.ino

--- Cscripts/main.ino
+++ Cscripts/main.ino
@@ -1,10 +1,23 @@
 #include "DAC_10ms.h"
+#include "ADC.h"
+
+const int adcPin = 34;
+const int dacPin = 25;
+unsigned long lastADCReadTime = 0;
+const unsigned long adcDelay = 1000;
 
 void setup() {
-    // 初始化代碼
+    Serial.begin(9600);
+    delay(1000);
 }
 
 void loop() {
-    dac();
-    // 其他循環代碼
+    dac(dacPin);
+    
+    unsigned long currentTime = millis();
+    if (currentTime - lastADCReadTime >= adcDelay) {
+        int value = readADC(adcPin);
+        Serial.println(value);
+        lastADCReadTime = currentTime;
+    }
 }
